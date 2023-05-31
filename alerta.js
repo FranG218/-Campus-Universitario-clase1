50d68d2e8bde8478bb7189b5d3f3ddc0a9ac3e17
FranG218 <franjg3@outlook.com>
2023-05-30 21:30:30 -0300

    tp_7 added

---
+++ alerta.js
@@ -0,0 +1,14 @@
+function mostrarIG(){
+    alert("Usted va a salir de nuestro sitio web");
+    location.href ="https://www.instagram.com/bibliotecaenro/";
+}
+    
+function mostrarFB(){
+    alert("Usted va a salir de la página");
+    location.href ="https://www.facebook.com/bibliotecaenro";
+}
+
+function mostrarYT(){
+    alert("Usted va a salir de la página");
+    location.href ="https://www.youtube.com/";
+}
